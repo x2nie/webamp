e65a8406fe32a3d90c6138c4c690acd6dd284cbb
Fathony <fathony@smart-leaders.net>
2022-03-13 20:10:59 +0700

    show what the sysregion is in dom

--- packages/webamp-modern-2/src/skin/makiClasses/Layer.ts
+++ packages/webamp-modern-2/src/skin/makiClasses/Layer.ts
@@ -64,7 +64,8 @@ export default class Layer extends GuiObj {
     // this._div.style.pointerEvents = this._sysregion==-2 || this._ghost? 'none' : 'auto';
     this._div.style.pointerEvents = this._isMouseTrap? 'auto': 'none';
     // this._div.style.pointerEvents = 'auto';
-    this._div.style.display = this._sysregion==-2? 'none' : 'block';
+    // this._div.style.display = this._sysregion==-2? 'none' : 'block';
+    this._sysregion!=0 && this._div.setAttribute('sysregion', this._sysregion.toString());
     this._div.style.display = 'block';
     // this._div.style.display = this._sysregion < 0 ? 'none' : 'block';
     this._div.style.overflow = "hidden";
