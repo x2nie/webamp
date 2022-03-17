77955efcd1cdaae7ce98869bc583977880eb0090
Fathony <fathony@smart-leaders.net>
2022-03-17 22:07:46 +0700

    css:clip-path aka. createRegion: top + bottom

--- packages/webamp-modern-2/src/clip_path.ts
+++ packages/webamp-modern-2/src/clip_path.ts
@@ -1,17 +1,26 @@
 import { Edges } from "./skin/Clippath";
 
+document.getElementById('clickable').onclick = ev=>{
+    alert('click on green!')
+}
+
 function main(){
     const oriImg = document.getElementById("img1");
     const img2 = new Image()
     img2.onload = (ev) =>{
-        const canvas = document.createElement('canvas');
+        // const canvas = document.createElement('canvas');
+        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
         canvas.width = img2.width;
         canvas.height = img2.height;
         const ctx = canvas.getContext("2d");
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
         ctx.drawImage(img2, 0,0)
         const edge = new Edges();
         edge.parseCanvasTransparency(canvas);
         document.getElementById('top').textContent = edge.top;
+        document.getElementById('bottom').textContent = edge.bottom;
+        document.getElementById('app').style.clipPath = `polygon(${edge.top}, ${edge.bottom})`;
     }
     img2.setAttribute('src', oriImg.getAttribute('src'))
     
--- packages/webamp-modern-2/src/skin/Clippath.ts
+++ packages/webamp-modern-2/src/skin/Clippath.ts
@@ -7,31 +7,60 @@ export class Edges {
     parseCanvasTransparency(canvas: HTMLCanvasElement) {
         const w = canvas.width;
         const h = canvas.height;
+        console.log(w,h)
         const ctx = canvas.getContext('2d');
         const points = [];
         const data = ctx.getImageData(0,0,w,h).data;
-        var x,y,lastX, lastY, first:boolean;
+        var x,y,lastX, lastY; 
+        var first:boolean, found:boolean, lastfounded:boolean;
 
         //? return true if not transparent
         function fine(ax,ay):boolean{
-            // return data[(ax + (ay * h))*4+3] != 0;
-            return data[(ax + (ay * h))*4+3] == 255;
+            return data[(ax + (ay * w))*4+3] != 0;
+            // return data[(ax + (ay * w))*4+3] == 255;
         }
 
         function post(ax:number, ay:number) {
-            // points.push(`${ax}px ${ay}px`)
-            points.push(`x:${ax} y:${ay}`)
+            points.push(`${ax}px ${ay}px`)
+            // points.push(`x:${ax} y:${ay}`)
         }
         
-        //top
+        //? top -------------------------------------------------
         lastY = 0; first=true;
         for (x = 0; x < w; x++) {       //? scan top, left->right
+            found = false;
             for(y = 0; y < h; y++){     //? find most top of non-transparent
+                if(fine(x,y)) {
+                    found = true;
+                    // if(!first && y!= lastY){
+                    //     post(lastX, lastY);
+                    // }
+                    if(first || y!= lastY || x==w-1){
+                        first = false;
+                        post(x,y);
+                        lastY = y;
+                    }
+                    break;
+                }
+            }
+            if(!found && lastfounded){
+                post(lastX, lastY);
+            }
+            lastX = x;
+            lastfounded = found;
+        }
+        this.top = points.join(', \n')
+
+        //? bottom -------------------------------------------------
+        points.splice(0, points.length)
+        lastY = h-1; first=true;
+        for (x = w-1; x >= 0; x--) {       //? scan bottom, right->left
+            for(y = h-1; y >=0; y--){     //? find most top of non-transparent
                 if(fine(x,y)) {
                     if(!first && y!= lastY){
                         post(lastX, lastY);
                     }
-                    if(first || y!= lastY || x==w-1){
+                    if(first || y!= lastY || x==0){
                         first = false;
                         post(x,y);
                         lastY = y;
@@ -41,6 +70,6 @@ export class Edges {
             }
             lastX = x;
         }
-        this.top = points.join(', \n')
+        this.bottom = points.join(', \n')
     }
 }
