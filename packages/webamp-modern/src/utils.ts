588442694b786c858fc078383893224b3ce3979f
Fathony <fathony@smart-leaders.net>
2022-05-11 11:11:02 +0700

    do not stop on wrong/unknown bool value

--- packages/webamp-modern/src/utils.ts
+++ packages/webamp-modern/src/utils.ts
@@ -51,7 +51,7 @@ export function relative(size: number): string {
 }
 
 export function toBool(str: string) {
-  assert(
+  assume(
     str === "0" || str === "1" ||
     str === "false" || str === "true",
     `Expected bool value to be "0" or "1", but it was "${str}".`
