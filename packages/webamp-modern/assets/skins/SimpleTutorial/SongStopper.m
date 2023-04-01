7dfd36644d457a833759730c06e797a1c13bf264
Fathony Luthfillah <fathony@smart-leaders.net>
2023-04-01 11:04:04 +0700

    PERFECTO !  it works like I want, + doesnt make winamp/wacup crash !

--- packages/webamp-modern/assets/skins/SimpleTutorial/SongStopper.m
+++ packages/webamp-modern/assets/skins/SimpleTutorial/SongStopper.m
@@ -1,4 +1,4 @@
-#include "..\..\lib/std.mi"
+#include <lib/std.mi>
 
 Global Text debugtext;
 Global Timer SongStop;
@@ -8,18 +8,22 @@ System.onScriptLoaded(){
     Group player = getScriptGroup();
     debugtext = player.findObject("debugtext");
     SongStop = new Timer;
-    SongStop.setDelay(0);
+    SongStop.setDelay(100);
 }
 
-System.onInfoChange(String info){
+// System.onInfoChange(String info){
+System.onPlay(){
+    SongStop.start();
+}
+System.onResume(){
     SongStop.start();
 }
 
 SongStop.onTimer(){
-    if(getPosition() >= 5000){
-        System.pause();
+    // if(getPosition() >= 5000){
         SongStop.stop();
-    }
+        System.pause();
+    // }
     debugtext.setText(integerToString(getPosition()));
 }
 
