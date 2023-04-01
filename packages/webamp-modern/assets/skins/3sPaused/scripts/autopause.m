6bcd88e6791ef079db6ff43bdfd68c72b93bba9d
Fathony Luthfillah <fathony@smart-leaders.net>
2023-04-01 15:42:01 +0700

    we force immediate pause twice (I assumed it accurate & audio data is available)

--- packages/webamp-modern/assets/skins/3sPaused/scripts/autopause.m
+++ packages/webamp-modern/assets/skins/3sPaused/scripts/autopause.m
@@ -5,16 +5,25 @@
 #include <lib/std.mi>
 #include <lib/application.mi>
 
-Global Timer mainTimer;
-Global Timer posTimer;
+Global Timer mainTimer; // pause on around 3000mx
+Global Timer tailTimer; //  to pump audio data at last 100ms
+Global Timer posTimer;  // show real position of audio
 Global Text positionText;
+// Global Slider seekSlider;
 
 System.onScriptLoaded() {
   Group player = getScriptGroup();
 
+  // seekSlider = player.findObject("player.slider.seek");
+
   Text appTitleText;
   appTitleText = player.findObject("apptitle");
   appTitleText.setText(Application.GetApplicationName());
+
+
+  Vis playerVis;
+  playerVis = player.findObject("vis");
+  playerVis.setRealtime(1);
 
   positionText = player.findObject("position");
   posTimer = new Timer;
@@ -24,6 +33,9 @@ System.onScriptLoaded() {
   mainTimer = new Timer;
   mainTimer.setDelay(3000);
   //mainTimer.stop();
+
+  tailTimer = new Timer;
+  tailTimer.setDelay(200);
 }
 
 System.onScriptUnloading() {
@@ -42,10 +54,21 @@ System.onResume(){
 }
 
 mainTimer.onTimer(){
-    System.pause();
+    // System.pause();
     mainTimer.stop();
+    // seekSlider.setPosition(integerToString(getPosition()))
+    System.seekTo(integer(getPosition() / 1000) * 1000 - 200);
+    tailTimer.start();
+}
+
+tailTimer.onTimer(){
+    // positionText.setText(integerToString(getPosition()));
+    System.pause();
+    tailTimer.stop();
+    System.seekTo(integer(getPosition() / 1000) * 1000);
+
 }
 
 posTimer.onTimer(){
-    positionText.setText(integerToString(getPosition()));
+    positionText.setText(integerToString(getPosition())) ;
 }
