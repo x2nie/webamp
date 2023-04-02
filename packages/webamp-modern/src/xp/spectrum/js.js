c458da5d53014cb963e8e0d2457fda492c7b49bf
Fathony Luthfillah <fathony@smart-leaders.net>
2023-04-02 22:48:55 +0700

    new spectrum comparison app

---
+++ packages/webamp-modern/src/xp/spectrum/js.js
@@ -0,0 +1,101 @@
+// taken from: https://codepen.io/nfj525/pen/rVBaab
+window.onload = function() {
+  
+    var file = document.getElementById("thefile");
+    var audio = document.getElementById("audio");
+    var preview = document.getElementById("preview");
+
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    // var ctx = canvas.getContext("2d");
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    const buttons = document.getElementsByTagName('button');
+    // buttons.onclick = (e) => console.log('btn click!');
+    // console.log(buttons)
+    for(const btn of buttons) {
+      btn.onclick = function(e){
+        // console.log('btn click', this)
+        // audio.stop();
+        preview.style.backgroundImage = `url(preview/${this.attributes.media.value}.png)`;
+        audio.src = `/assets/audio/check-sound/${this.attributes.media.value}.wav`;
+        audio.play();
+      }
+    };
+    
+    file.onchange = function() {
+      var files = this.files;
+      audio.src = URL.createObjectURL(files[0]);
+      audio.load();
+      audio.play();
+    }
+
+    audio.onplay = function (e) {
+      // var context = new AudioContext();
+      // var src = context.createMediaElementSource(audio);
+      // var analyser = context.createAnalyser();
+  
+      // var canvas = document.getElementById("canvas");
+      // canvas.width = window.innerWidth;
+      // canvas.height = window.innerHeight;
+      var ctx = canvas.getContext("2d");
+  
+      // src.connect(analyser);
+      // analyser.connect(context.destination);
+  
+      // analyser.fftSize = 256;
+  
+      // var bufferLength = analyser.frequencyBinCount;
+      // console.log(bufferLength);
+  
+      // var dataArray = new Uint8Array(bufferLength);
+  
+      var WIDTH = canvas.width;
+      var HEIGHT = canvas.height;
+  
+      var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+  
+      function renderFrame() {
+        requestAnimationFrame(renderFrame);
+  
+        x = 0;
+  
+        analyser.getByteFrequencyData(dataArray);
+  
+        ctx.fillStyle = "#00f";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  
+        for (var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+          
+          var r = barHeight + (25 * (i/bufferLength));
+          var g = 250 * (i/bufferLength);
+          var b = 50;
+  
+          ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+          ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+  
+          x += barWidth + 1;
+        }
+      }
+  
+      // audio.play();
+      renderFrame();
+    };
+  };
