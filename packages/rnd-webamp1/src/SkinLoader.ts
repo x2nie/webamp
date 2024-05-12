8d4810d3564651439bcc1de76cff6fa7b5cc2dd4
Fathony L <fathony@smart-leaders.net>
2024-05-12 18:46:39 +0700

    solving fatal error: null.children

--- packages/rnd-webamp1/src/SkinLoader.ts
+++ packages/rnd-webamp1/src/SkinLoader.ts
@@ -14,8 +14,8 @@ export class SkinLoader {
     response = await fetch(skinPath);
     await this.fileExtractor.prepare(skinPath, response);
     await this.parseSkin();
-    const tpl = this._containers.join('\n')
-      console.log('FINAL-TPL---------------------------\n', tpl)
+    // const tpl = this._containers.join('\n')
+    //   console.log('FINAL-TPL---------------------------\n', tpl)
   }
 
   // async getFileAsString(path):string {
@@ -31,7 +31,7 @@ export class SkinLoader {
     // Note: Included files don't have a single root node, so we add a synthetic one.
     // A different XML parser library might make this unnessesary.
     const parsed = parseXml(includedXml) as unknown as XmlElement;
-    await this.traverseChildren(parsed);
+    await this.traverseChildren(parsed, parsed);
   }
 
   async traverseChildren(
@@ -74,6 +74,33 @@ export class SkinLoader {
     // }
   }
 
+  async traverseChilds(nodes: XmlElement[], parent: any, path: string[] = []) {
+    const elements = nodes.filter(el => el instanceof XmlElement)
+
+    // return await Promise.all(
+    //   elements.map((child) => this.traverseChild(child as XmlElement, parent, path))
+    // );
+
+    // if (this._phase == RESOURCE_PHASE) {
+    // return await Promise.all(
+    //   node.children.map((child) => {
+    //     if (child instanceof XmlElement) {
+    //       // console.log('traverse->', parent.name, child.name)
+    //       // this._scanRes(child);
+    //       return this.traverseChild(child, parent, path);
+    //     }
+    //   })
+    // );
+    // } else {
+      for (const child of elements) {
+        // if (child instanceof XmlElement) {
+          // this._scanRes(child);
+          await this.traverseChild(child, parent, path);
+        // }
+      }
+      return elements
+    // }
+  }
   async traverseChild(node: XmlElement, parent: any, path: string[] = []) {
     const tag = node.name.toLowerCase();
     switch (tag) {
@@ -126,19 +153,26 @@ export class SkinLoader {
     // console.log('include #3', fileName)
     
     
-    await this.traverseChildren(parsed, parent, [...path, ...directories]);
-    // console.log('include #4', fileName)
+    // await this.traverseChildren(parsed, parent, [...path, ...directories]);
+    // console.log('include #4>', fileName, parsed)
+    // debugger
+    const childs = await this.traverseChilds(parsed.children[0].children, parent, [...path, ...directories]);
+    // console.log('include #4<', fileName, parsed)
     
-
-    if(!parsed.children) {
-      console.log('parsed-HAS-NO CHILD:', parsed.toJSON())
+    // if(!parsed.children) {
+    //   console.log('parsed-HAS-NO CHILD:', parsed.toJSON())
+    //   return
+    // }
+    // console.log('include #4 ==>>', fileName, childs)
+    if(!parent || !parent.children) {
+      console.log('parent-HAS-NO CHILD:', parent.toJSON())
       return
     }
     // debugger
     // let childrens = parsed  ? parsed.children : 
     
     let replaced = false;
-    parsed.children.forEach(child => {
+    childs.forEach(child => {
       // console.log('include #5~', fileName, child.name, '#', child.toJSON())
       if(!replaced){
         node.name = child.name
@@ -163,12 +197,14 @@ export class SkinLoader {
     await this.traverseChildren(node, node, path);
     console.log(node.attributes.name,node.toJSON())
     // return node
-    if(!node.children) 
-      console.log('HAS-NO CHILD:', node.toJSON())
+    // if(!node.children) 
+    //   console.log('HAS-NO CHILD:', node.toJSON())
     const layouts = node.children.filter(el => el.name == 'layout').map(
-      l => `<${l.name} ${atts(l.attributes)}></${l.name}>`
+    // const layouts = getLayouts(node).map(
+      l => `<${l.name} ${atts(l.attributes)}/>`
+      // l => `<${l.name} ${atts(l.attributes)}></${l.name}>`
     )
-    const tpl = `<Container ${atts(node.attributes)}>\n${layouts.join('\n')}</Container>`
+    const tpl = `<Container ${info(node.attributes)}>\n\t${layouts.join('\n\t')}</Container>`
     // console.log(node.attributes.name,tpl)
     this._containers.push(tpl);
   }
@@ -186,7 +222,7 @@ function parseXmlFragment(xml: string): XmlElement {
   return parseXml(`<wrapper>${xml}</wrapper>`) as unknown as XmlElement;
 }
 
-function atts(attributes:{[attrName: string]: string}):string{
+function info(attributes:{[attrName: string]: string}):string{
   const def = {
     id: attributes.id,
     x: attributes['default_x'] || Math.round(Math.random() * (window.innerWidth - 50)),
@@ -195,12 +231,26 @@ function atts(attributes:{[attrName: string]: string}):string{
     height: 50,
   }
   return `info="${JSON.stringify(def).replaceAll('"', "'")}"`
-  
-  return `id="'${attributes.id}'" info=`
+}
+function atts(attributes:{[attrName: string]: string}):string{
+  // return `id="'${attributes.id}'" info=`
   const result:string[] = []
   for(const [k,v] of Object.entries(attributes)){
     if(k=='component') continue
     result.push(`${k}="${v}"`)
   }
   return result.join(' ')
+}
+
+function getLayouts(node:XmlElement): XmlElement[] {
+  const ret = []
+  const recursive = (n:XmlElement) =>{
+    if(n.name=='layout') {
+      ret.push(n)
+    } else if(n.children) {
+      n.children.forEach(c => recursive(c))
+    }
+  }
+  recursive(node)
+  return ret
 }
