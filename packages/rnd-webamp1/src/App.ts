2d3d771127bd64a4d8950f0f2bc8f67a341398e6
Fathony L <fathony@smart-leaders.net>
2024-05-12 09:42:33 +0700

    cleanup

--- packages/rnd-webamp1/src/App.ts
+++ packages/rnd-webamp1/src/App.ts
@@ -66,35 +66,6 @@ class Window extends Component {
     this.updateZIndex();
     if(ev.button!=0) return;
     this.windowService.handleMouseDown(this.props.info.id, ev)
-    return;
-    const self = this;
-    const root = this.root;
-
-    const el = root.el;
-    el.classList.add("dragging");
-
-    const current = this.props.info;
-    const offsetX = current.left - ev.pageX;
-    const offsetY = current.top - ev.pageY;
-    let left, top;
-
-    window.addEventListener("mousemove", moveWindow);
-    window.addEventListener("mouseup", stopDnD, { once: true });
-
-    function moveWindow(ev) {
-      left = Math.max(offsetX + ev.pageX, 0);
-      top = Math.max(offsetY + ev.pageY, 0);
-      el.style.left = `${left}px`;
-      el.style.top = `${top}px`;
-    }
-    function stopDnD() {
-      window.removeEventListener("mousemove", moveWindow);
-      el.classList.remove("dragging");
-
-      if (top !== undefined && left !== undefined) {
-        self.windowService.updatePosition(current.id, left, top);
-      }
-    }
   }
 
   updateZIndex() {
