66ea53a6e46d15ce9b5097ad00514c44a760dee5
Fathony L <fathony@smart-leaders.net>
2024-05-18 00:49:22 +0700

    Layer: inherit style from guiobj

--- packages/rnd-webamp1/src/skin/GuiObject.ts
+++ packages/rnd-webamp1/src/skin/GuiObject.ts
@@ -3,23 +3,84 @@ import { Object_ } from "./Object";
 import { value2number } from "@xml/parse-xml";
 
 // http://wiki.winamp.com/wiki/XML_GUI_Objects#GuiObject_.28Global_params.29
-export class GuiObject extends Object_{
-    static GUID = "4ee3e1994becc636bc78cd97b028869c";
+export class GuiObject extends Object_ {
+  static GUID = "4ee3e1994becc636bc78cd97b028869c";
 
-    setup() {
-        this.env = useEnv()
+  setup() {
+    this.env = useEnv();
+  }
+  get att() {
+    return this.props.node.attributes;
+  }
+  setXmlParam(param: string, value: string) {
+    if (value2number.includes(param)) {
+      //@ts-ignore
+      value = Number(value);
     }
-    get att(){
-        return this.props.node.attributes
-    }
-    setXmlParam(param :string, value :string){
-        if(value2number.includes(param)){
-            //@ts-ignore
-            value = Number(value)
-        }
-        this.att[param] = value
-    }
-    getXmlParam(param :string) :string{
-        return String(this.att[param])
-    }
+    this.att[param] = value;
+  }
+  getXmlParam(param: string): string {
+    return String(this.att[param]);
+  }
+
+  getId(): string {
+    return this.att.id || "";
+  }
+
+  style(){
+    let {x,y, alpha, visible } = this.att;
+    let style = `top:${y}px; left:${x}px; color:fuchsia;`;
+    if(alpha!=null && alpha < 255) style += `opacity:${alpha / 255};`
+    if(visible!=null && !visible) style += `display:none;`
+    return style
+  }
+  /**
+   * Trigger the show event.
+   */
+  show() {
+    this.att.visible = true;
+  }
+
+  /**
+   * Trigger the hide event.
+   */
+  hide() {
+    this.att.visible = false;
+  }
+  isVisible(): boolean {
+    return this.att.visible;
+  }
+
+  /** getter setter */
+  get visible(): boolean {
+    return this.att.visible;
+  }
+  set visible(showing: boolean) {
+    this.att.visible = showing
+  }
+
+
+  /**
+   * Set the alphablending value of the object.
+   * Value ranges from 0 (fully transparent) to
+   * 255 (fully opaque).
+   *
+   * @param  alpha   The alpha value.
+   */
+  setAlpha(alpha: number) {
+    this.att.alpha = alpha;
+    // this._renderAlpha();
+  }
+
+  /**
+   * Get the current alphablending value of
+   * the object. Value ranges from 0 (fully
+   * transparent) to 255 (fully opaque).
+   *
+   * @ret The alpha value.
+   */
+  getAlpha(): number {
+    const alpha = this.att.alpha
+    return alpha!==null ? alpha : 255;
+  }
 }
--- packages/rnd-webamp1/src/skin/Layer.ts
+++ packages/rnd-webamp1/src/skin/Layer.ts
@@ -1,24 +1,25 @@
 import { Component, xml, onMounted, useEnv, useRef } from "@odoo/owl";
 import { registry } from '@web/core/registry';
+import { GuiObject } from "./GuiObject";
 
-export class Layer extends Component {
+export class Layer extends GuiObject {
     // @<t t-out="att.tag"/> :
     //   <t t-out="att.id"/>
     // <t t-slot="default"/>
     static template = xml`
-    <div class="layer" t-att-id="att.id" t-att-style="style">
+    <div class="layer" t-att-id="att.id" t-att-style="style()">
     </div>`;
 
-    setup() {
-        this.env = useEnv()
-    }
-    get att(){
-        return this.props.node.attributes
-    }
+    // setup() {
+    //     this.env = useEnv()
+    // }
+    // get att(){
+    //     return this.props.node.attributes
+    // }
     
-    get style() {
-        let {  y, x } = this.att;
-        let style = `top:${y}px; left:${x}px; color:fuchsia;`;
+    style() {
+        let style = super.style()
+        // let { image } = this.att;
         if(this.att.image){
             // const url = this.env.bitmaps[this.att.image].url
             const bitmap = this.env.ui.bitmaps[this.att.image]
